09958ce926b8c7bc257e1652a6781b11d6006296
davelaurengerencia <99423438+davelaurengerencia@users.noreply.github.com>
2023-09-27 15:50:09 +1000

    Initial commit

---
+++ scripts/postInstall.sh
@@ -0,0 +1,9 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#wait until the server is ready
+echo "Waiting for software to be ready ..."
+sleep 60s;
+
+
+  docker-compose exec -T baserow curl -X POST -H 'Content-Type: application/json' -i http://localhost/api/user/ --data '{ "name": "Admin", "email": "'${ADMIN_EMAIL}'", "password": "'${ADMIN_PASSWORD}'" }'
